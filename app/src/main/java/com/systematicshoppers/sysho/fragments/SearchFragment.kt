99a4b91c4e8756cceb97c5974c5b8d07ead2136a
Coreyander <coalex89@gmail.com>
2023-02-07 18:18:29 -0500

    Added logo

--- app/src/main/java/com/systematicshoppers/sysho/fragments/SearchFragment.kt
+++ app/src/main/java/com/systematicshoppers/sysho/fragments/SearchFragment.kt
@@ -7,11 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.systematicshoppers.sysho.R
 import com.systematicshoppers.sysho.activities.MainActivity
 
@@ -22,10 +24,16 @@ class SearchFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_search, container, false)
         val shopBtn = view.findViewById<Button>(R.id.shopBtn)
+        val navBar: BottomNavigationView? = activity?.findViewById(R.id.bottomNavigationView)
+        navBar?.isVisible = false
 
         shopBtn.setOnClickListener {
-            view.findNavController().navigate(SearchFragmentDirections.actionSearchFragmentToResultsFragment())
+            navBar?.isVisible = true
+            view.findNavController().navigate(R.id.action_searchFragment_to_resultsFragment)
         }
+
+
+
         return view
     }
 }
