b4603760632e891641118237b3bbeec5e07035e6
ayeshatahamid <a.tahamid@ufl.edu>
2023-03-09 11:00:27 -0500

    add half of checkbox functionality

--- app/src/main/java/com/systematicshoppers/sysho/fragments/ResultsFragment.kt
+++ app/src/main/java/com/systematicshoppers/sysho/fragments/ResultsFragment.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import com.systematicshoppers.sysho.R
 
 class ResultsFragment : Fragment() {
@@ -14,6 +17,22 @@ class ResultsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_results, container, false)
+
+        val linearLayout = view.findViewById<LinearLayout>(R.id.repeatList)
+        val data = arguments?.getStringArrayList("storedList")?.toMutableList()
+        if (data != null) {
+            for (item in data) {
+                val textView = TextView(this.context)
+                textView.text = item
+                textView.layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+                linearLayout.addView(textView)
+            }
+        }
+
+
         return view
     }
 }
--- app/src/main/java/com/systematicshoppers/sysho/fragments/SearchFragment.kt
+++ app/src/main/java/com/systematicshoppers/sysho/fragments/SearchFragment.kt
@@ -2,6 +2,7 @@
 package com.systematicshoppers.sysho.fragments
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.LayoutInflater
@@ -9,8 +10,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.widget.*
+import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResult
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -30,40 +33,98 @@ class SearchFragment : Fragment() {
         val navBar: BottomNavigationView? = activity?.findViewById(R.id.bottomNavigationView)
         navBar?.isVisible = false
 
+        //grocery list for selected item
+        var storedList: MutableList<String> = mutableListOf("")
+        var anyChecked = false
+        val deleteList = mutableListOf<CheckBox>()
+        var count = 0
+        var checkBoxList = mutableListOf<CheckBox>()
+        val groceryList = view.findViewById<LinearLayout>(R.id.grocery_list)
+        val scroll_view = view.findViewById<ScrollView>(R.id.scroll_view)
+
+        //button to go to results fragment
         shopBtn.setOnClickListener {
-            navBar?.isVisible = true
-            view.findNavController().navigate(R.id.action_searchFragment_to_resultsFragment)
+            if (!anyChecked) {
+                navBar?.isVisible = true
+                val fragmentB = ResultsFragment()
+                val bundle = Bundle()
+                bundle.putStringArrayList("storedList", ArrayList(storedList))
+                fragmentB.arguments = bundle
+
+                parentFragmentManager.beginTransaction()
+                    .addToBackStack(null)
+                    .commit()
+                view.findNavController().navigate(R.id.action_searchFragment_to_resultsFragment)
+            }
+            else {
+                for (del in deleteList) {
+                    groceryList.removeView(del)
+                }
+            }
         }
 
-        //stand in "database" for testing
+        //stand in "database" for testing autocomplete
         var database: MutableList<String> = mutableListOf("apple", "orange", "banana", "orangutan")
 
-        //autofill for search bar
+        //create search bar w autofill
+        val searchbar = view.findViewById<AutoCompleteTextView>(R.id.search_bar)
         val adapter = activity?.let {
             ArrayAdapter<String>(it, android.R.layout.simple_list_item_1, database)
         }
-
-        val searchbar = view.findViewById<AutoCompleteTextView>(R.id.search_bar)
         searchbar.setAdapter(adapter)
 
-        //grocery list for selected item
-        var groceryList: MutableList<String> = mutableListOf("apple")
-
-
         fun addItemToList(autoCompleteTextView: AutoCompleteTextView) {
-            autoCompleteTextView.setOnEditorActionListener { textView, actionId, keyEvent ->
+            autoCompleteTextView.setOnEditorActionListener { grocery_item, actionId, keyEvent ->
                 if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    val item = view.findViewById<TextView>(R.id.item1)
                     if (searchbar.text.isNotEmpty()) {
-                        //add item to mutable list
-//                        val entry: String = editText.text.toString()
-//                        groceryList.add(index, entry)
-//                        index++
+                        //need a smaller row layout to have checkbox next to text
+                        val rowLayout = LinearLayout(this.context)
+                        rowLayout.orientation = LinearLayout.HORIZONTAL
 
-                        //update the items with the input item
-                        item.text = searchbar.text.toString()
+                        //adds new item onto fragment list
+                        val checkBox = CheckBox(this.context)
+                        checkBox.text = searchbar.text
+                        checkBoxList.add(checkBox)
+                        count++
+                        groceryList.addView(checkBox)
+
+                        //adds new item onto mutable grocery list
+                        storedList.add(searchbar.text.toString())
+
+                        //focuses on item added to bottom of list
+                        scroll_view.fullScroll(View.FOCUS_DOWN)
                         searchbar.text.clear()
                     }
+                    //if anything is checked, change button and change to delete mode
+                    for (toDelete in checkBoxList) {
+                        toDelete.setOnCheckedChangeListener { _, isChecked ->
+                            if (toDelete.isChecked) {
+                                anyChecked = true
+                                deleteList.add(toDelete)
+                                shopBtn.text = "Delete"
+                                shopBtn.setBackgroundResource(R.color.red)
+                                Toast.makeText(this.context, "Checked!", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+
+
+//                    for (toDelete in checkBoxList) {
+//                        toDelete.setOnCheckedChangeListener { _, isChecked ->
+//                            if (anyChecked) {
+//                                shopBtn.text = "Delete"
+//                                shopBtn.setBackgroundResource(R.color.red)
+//                                //Toast.makeText(this.context, "anyChecked True", Toast.LENGTH_SHORT).show()
+//                            }
+//                            else {
+//                                shopBtn.text = "Ready to Shop"
+//                                shopBtn.setBackgroundResource(R.color.green_400_dark)
+//                                //Toast.makeText(this.context, "anyChecked False", Toast.LENGTH_SHORT).show()
+//                            }
+//                        }
+//                    }
+
+
                     true
                 } else {
                     false
@@ -71,12 +132,22 @@ class SearchFragment : Fragment() {
             }
         }
 
+        fun deleteItems() {
+            for (i in 1..checkBoxList.size) {
+                val checkBoxtoDelete = view.findViewById<CheckBox>(i)
+                if (checkBoxtoDelete.isChecked) {
+                    groceryList.removeView(checkBoxtoDelete)
+                }
+            }
+        }
 
 
         //when clicking, to do with search bar
         searchbar.setOnClickListener {
             addItemToList(searchbar)
         }
+
+
 
         return view
     }
