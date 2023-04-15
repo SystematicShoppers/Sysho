b430662bbc6a40abe320cf34cdb5019c40bbe22c
Derek Pearson <dpearson2699@gmail.com>
2023-04-15 18:06:44 -0400

    Changes by coale

--- app/src/main/java/com/systematicshoppers/sysho/database/ListItem.kt
+++ app/src/main/java/com/systematicshoppers/sysho/database/ListItem.kt
@@ -1,6 +1,8 @@
 package com.systematicshoppers.sysho.database
 
+import com.google.firebase.firestore.PropertyName
+
 data class ListItem (
-    var entry: String = "",
-    var quantity: Int = 0
-        )
+    @get:PropertyName("entry") @set:PropertyName("entry") var entry: String = "",
+    @get:PropertyName("quantity") @set:PropertyName("quantity") var quantity: Int = 0
+        ) : java.io.Serializable
