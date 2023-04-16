f5846a4909da580e85f0b7c33e46fd2b29fc87e4
ayeshatahamid <a.tahamid@ufl.edu>
2023-04-16 15:51:03 -0400

    add another test for distance toggle

--- app/src/androidTest/java/com/systematicshoppers/sysho/ResultsFragmentTest.kt
+++ app/src/androidTest/java/com/systematicshoppers/sysho/ResultsFragmentTest.kt
@@ -1,5 +1,4 @@
 // ResultsFragmentTest.kt
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -8,7 +7,6 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.systematicshoppers.sysho.activities.MainActivity
-import com.systematicshoppers.sysho.fragments.ResultsFragment
 import com.systematicshoppers.sysho.R
 import com.systematicshoppers.sysho.database.QueryItem
 import org.junit.Rule
@@ -22,7 +20,7 @@ class ResultsFragmentTest {
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun launchResultsFragment_checkViewsDisplayed() {
+    fun checkViewsDisplayedInResultsFragment() {
         val itemsToAdd = listOf("apples", "bananas", "oranges")
 
         // Add items to the searchFragmentRecyclerView and create a queryList
@@ -34,8 +32,34 @@ class ResultsFragmentTest {
             onView(withId(R.id.search_bar)).perform(ViewActions.closeSoftKeyboard())
             queryList.add(QueryItem(item, false, 1))
         }
-
+        // Go to Results Page
         onView(withId(R.id.shopBtn)).perform(ViewActions.click())
+
+        onView(withId(R.id.resultsRecyclerView)).check(matches(isDisplayed()))
+        onView(withId(R.id.filterDistance)).check(matches(isDisplayed()))
+        onView(withId(R.id.filterPrice)).check(matches(isDisplayed()))
+        onView(withId(R.id.filterTextView)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun displayDistance() {
+        val itemsToAdd = listOf("apples", "bananas")
+
+        // Add items to the searchFragmentRecyclerView and create a queryList
+        val queryList: MutableList<QueryItem> = mutableListOf()
+        for (item in itemsToAdd) {
+            onView(withId(R.id.search_bar)).perform(ViewActions.click())
+            onView(withId(R.id.search_bar)).perform(ViewActions.typeText(item))
+            onView(withId(R.id.search_bar)).perform(ViewActions.pressImeActionButton())
+            onView(withId(R.id.search_bar)).perform(ViewActions.closeSoftKeyboard())
+            queryList.add(QueryItem(item, false, 1))
+        }
+        onView(withId(R.id.shopBtn)).perform((ViewActions.click()))
+        Thread.sleep(2000)
+
+        // Click the distance button when the Results page is displayed
+        onView(withId(R.id.filterDistance)).perform(ViewActions.click())
+
         onView(withId(R.id.resultsRecyclerView)).check(matches(isDisplayed()))
         onView(withId(R.id.filterDistance)).check(matches(isDisplayed()))
         onView(withId(R.id.filterPrice)).check(matches(isDisplayed()))
