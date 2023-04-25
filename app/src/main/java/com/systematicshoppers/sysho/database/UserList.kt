5999141e14d407045d3cc38be9d680b8171c574e
Derek Pearson <dpearson2699@gmail.com>
2023-04-25 00:54:27 -0400

    Formatting

--- app/src/main/java/com/systematicshoppers/sysho/database/UserList.kt
+++ app/src/main/java/com/systematicshoppers/sysho/database/UserList.kt
@@ -2,8 +2,9 @@ package com.systematicshoppers.sysho.database
 
 /**
  * UserList is a data class that represents a user's shopping list. It has two properties:
- * id: A String representing the unique identifier of the list. It is initialized with an empty string by default.
+ * id: A String representing the unique identifier of the list. It is initialized with an empty string.
  * items: A List of ListItem objects representing the individual items in the shopping list. It is initialized with an empty list by default.
+ *
  * UserList implements the Serializable interface so that it can be passed between activities.
  */
 data class UserList(
