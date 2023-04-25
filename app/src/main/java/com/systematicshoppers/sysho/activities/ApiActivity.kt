18163a24e57f4a400ee8c0ec0ce4ddcec40e2ad5
Derek Pearson <dpearson2699@gmail.com>
2023-04-25 00:40:22 -0400

    Added comments to the ApiActivity.kt

--- app/src/main/java/com/systematicshoppers/sysho/activities/ApiActivity.kt
+++ app/src/main/java/com/systematicshoppers/sysho/activities/ApiActivity.kt
@@ -8,35 +8,48 @@ import androidx.navigation.ui.setupWithNavController
 import com.systematicshoppers.sysho.R
 import com.systematicshoppers.sysho.databinding.ActivityApiBinding
 
+/**
+ * ApiActivity is an AppCompatActivity that serves as a container for the fragments
+ * involved in API-related operations
+ */
 class ApiActivity: AppCompatActivity() {
+    // Declare variables for the binding and NavController
     private lateinit var binding: ActivityApiBinding
     private lateinit var navController: NavController
 
+    // Function called when the activity is created
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Inflate the ActivityApiBinding and set the content view to the generated view hierarchy
         val binding = ActivityApiBinding.inflate(layoutInflater)
         val viewRoot = binding.root
         setContentView(viewRoot)
 
+        // Retrieve the NavHostFragment and NavController associated with this activity
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.content) as NavHostFragment
         navController = navHostFragment.navController
 
+        // Set up the bottom navigation view with the NavController
         val nav = binding.bottomNavigationView
         nav.setupWithNavController(navController)
 
+        // Set an item selected listener for the bottom navigation view
         nav.setOnItemSelectedListener { item ->
+            // Determine the action to perform based on the selected item's ID
             when (item.itemId) {
                 R.id.apiStoreFragment -> {
+                    // Pop back to the root of the navigation graph, then navigate to the apiStoreFragment
                     navController.popBackStack()
                     navController.navigate(R.id.apiStoreFragment)
                     true
                 }
                 R.id.apiProductFragment -> {
+                    // Pop back to the root of the navigation graph, then navigate to the apiProductFragment
                     navController.popBackStack()
                     navController.navigate(R.id.apiProductFragment)
                     true
                 }
-                else -> false
+                else -> false// Return false for any other unhandled item IDs
             }
         }
 
