73e37a94f1369b81cfafc55f1c3b3c62e1d88d13
Derek Pearson <dpearson2699@gmail.com>
2023-04-25 17:37:33 -0400

    Added more comments to ApiProductFragment.kt

--- app/src/main/java/com/systematicshoppers/sysho/fragments/ApiProductFragment.kt
+++ app/src/main/java/com/systematicshoppers/sysho/fragments/ApiProductFragment.kt
@@ -15,19 +15,27 @@ import com.systematicshoppers.sysho.SyshoViewModel
 import com.systematicshoppers.sysho.adapters.ApiProductsAdapter
 import com.systematicshoppers.sysho.database.FirebaseUtils
 import com.systematicshoppers.sysho.database.Product
-/**Fragment for ApiActivity to display the products database and handle the click events of its RecyclerView.**/
+
+/**
+ * Fragment for ApiActivity to display the products from the Firestore database
+ * and handle the click events of its RecyclerView.
+ */
 class ApiProductFragment : Fragment(), ApiProductsAdapter.ClickListener {
 
     private lateinit var apiProductsRecyclerView: RecyclerView
     private lateinit var apiProductsAdapter: ApiProductsAdapter
     private val viewModel: SyshoViewModel by activityViewModels()
 
+    /**
+     * Inflate the layout and set up the RecyclerView with its adapter and layout manager.
+     */
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_api_product, container, false)
 
+        // Retrieve the list of products from Firebase, and initialize the RecyclerView and its adapter.
         getProducts { products ->
             if(isAdded) {
                 apiProductsAdapter = ApiProductsAdapter(requireContext(), products, this)
@@ -41,24 +49,33 @@ class ApiProductFragment : Fragment(), ApiProductsAdapter.ClickListener {
         return view
     }
 
+    /**
+     * Handle the click event for a product item in the RecyclerView.
+     */
     override fun gotoProduct(position: Int, productData: Product) {
+        // Set the selected product data in the ViewModel.
         viewModel.setProductData(productData)
+        // Replace the current fragment with ApiProductSelectFragment.
         parentFragmentManager.beginTransaction()
             .replace(R.id.content, ApiProductSelectFragment())
             .addToBackStack(null)
             .commit()
     }
 
-    /**Returns through a callback with a list of Products parsed from Firebase documents.**/
+    /**
+     * Returns through a callback with a list of Products parsed from Firebase documents.
+     */
     private fun getProducts(callback: (MutableList<Product>) -> Unit) {
         FirebaseUtils().fireStoreDatabase.collection("products")
             .get()
             .addOnSuccessListener { querySnapshot ->
                 val products = mutableListOf<Product>()
+                // Iterate through the documents in the querySnapshot and add them to the products list.
                 querySnapshot.forEach { productDocument ->
                     Log.d(TAG, "Read document with ID ${productDocument.id}")
                     products.add(productDocument.toObject(Product::class.java))
                 }
+                // Invoke the callback function with the retrieved products list.
                 callback(products)
             }
     }
