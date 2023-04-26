dc341cd04cf5ac740b10c29ba7a10dcdfe641fe5
Derek Pearson <dpearson2699@gmail.com>
2023-04-25 21:03:38 -0400

    Added more comments to SyshoViewModel.kt

--- app/src/main/java/com/systematicshoppers/sysho/SyshoViewModel.kt
+++ app/src/main/java/com/systematicshoppers/sysho/SyshoViewModel.kt
@@ -4,21 +4,41 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.systematicshoppers.sysho.database.*
-/**A central ViewModel for the entire app to communicate data between Fragments. ViewModels are a special structure where data can be store
- * instead of sent as args between Fragments. ViewModel data lives with the Activity and can exchange data between fragments of that Activity.**/
+
+/**
+ * A central ViewModel for the entire app to communicate data between Fragments.
+ * ViewModels are a special structure where data can be stored instead of sent as args between Fragments.
+ * ViewModel data lives with the Activity and can exchange data between fragments of that Activity.
+ */
 class SyshoViewModel : ViewModel() {
 
+    // Mutable live data properties for various data types
     private var _stores = MutableLiveData<List<Store>>()
     val stores: LiveData<List<Store>> = _stores
+
+    /**
+     * Sets the value of the _stores MutableLiveData.
+     * @param passStores A list of Store objects.
+     */
     fun setStoresListData(passStores: List<Store>) {
         _stores.value = passStores
     }
 
+
+    /**
+     * Sets the value of the _store MutableLiveData.
+     * @param passStore A Store object.
+     */
     private var _store = MutableLiveData<Store>()
     val store: LiveData<Store> = _store
     fun setStoreData(passStore: Store) {
         _store.value = passStore
     }
+
+    /**
+     * Reloads the data of the store from Firestore based on the given documentId.
+     * @param documentId The document ID of the store in the Firebase Firestore collection.
+     */
     fun reloadStoreData(documentId: String?) {
         if (documentId != null) {
             FirebaseUtils().fireStoreDatabase.collection("stores").document(documentId)
@@ -32,12 +52,20 @@ class SyshoViewModel : ViewModel() {
         }
     }
 
+    /**
+     * Sets the value of the _product MutableLiveData.
+     * @param passProduct A Product object.
+     */
     private var _product = MutableLiveData<Product>()
     val product: LiveData<Product> = _product
     fun setProductData(passProduct: Product) {
         _product.value = passProduct
     }
 
+    /**
+     * Sets the value of the _products MutableLiveData.
+     * @param passProducts A list of Product objects.
+     */
     private var _products = MutableLiveData<List<Product>>()
     val products: LiveData<List<Product>> = _products
     fun setProductListData(passProducts: List<Product>) {
@@ -91,13 +119,22 @@ class SyshoViewModel : ViewModel() {
         _currentLat.value = passLat
     }
 
-    /**ResultsFragment callbacks **/
+    /**ResultsFragment callbacks**/
+    /**
+     * Sets the value of the _loadCoordinatesCallback MutableLiveData.
+     * @param result A Boolean value to indicate the result of loading coordinates.
+     */
     private val _loadCoordinatesCallback = MutableLiveData<Boolean>()
     val loadCoordinatesCallback: LiveData<Boolean> = _loadCoordinatesCallback
     fun loadCoordinatesCallback(result: Boolean) {
         _loadCoordinatesCallback.value = result
     }
 
+    /**
+     * Sets the value of the _totalPriceCallback MutableLiveData and updates the totalPrice MutableLiveData.
+     * @param result A Boolean value to indicate the result of calculating the total price.
+     * @param total The calculated total price.
+     */
     private val _totalPriceCallback = MutableLiveData<Boolean>()
     val totalPriceCallback: LiveData<Boolean> = _totalPriceCallback
     fun totalPriceCallback(result: Boolean, total: Double) {
@@ -120,7 +157,12 @@ class SyshoViewModel : ViewModel() {
     }
 
     /** Api Updates **/
-    /** Boolean toggles to trigger observer. Simple function to reset adapter. **/
+    /**
+     * Boolean toggles to trigger observer. Simple function to reset adapter
+     *
+     * Sets the value of the _apiStoreAdapterNotice MutableLiveData.
+     * @param passNotice A Boolean value to indicate the adapter notice.
+     */
     private val _apiStoreAdapterNotice = MutableLiveData<Boolean>()
     val apiStoreAdapterNotice: LiveData<Boolean> = _apiStoreAdapterNotice
     @Suppress("RedundantIf")
@@ -129,6 +171,10 @@ class SyshoViewModel : ViewModel() {
     }
 
     /** Navigation **/
+    /**
+     * Sets the value of the _currentFragment MutableLiveData.
+     * @param passCurrentFragment A String value indicating the current fragment.
+     */
     private val _currentFragment = MutableLiveData<String>()
     val currentFragment: LiveData<String> = _currentFragment
     fun setCurrentFragment(passCurrentFragment: String) {
