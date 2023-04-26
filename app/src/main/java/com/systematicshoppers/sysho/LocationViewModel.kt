cb0ff57cfb9e88c197fc1bbdb8a41e2d316f2284
Derek Pearson <dpearson2699@gmail.com>
2023-04-25 20:53:41 -0400

    Added more comments to LocationViewModel.kt

--- app/src/main/java/com/systematicshoppers/sysho/LocationViewModel.kt
+++ app/src/main/java/com/systematicshoppers/sysho/LocationViewModel.kt
@@ -7,18 +7,26 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
-
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-/**A view model for location services. Updates if there is any change in location caught by the LocationListener.**/
+
+/**
+ * A view model for location services.
+ * Updates if there is any change in location caught by the LocationListener.
+ */
 class LocationViewModel : ViewModel(), LocationListener {
+    // Declare variables for location-related information
     private lateinit var locationManager: LocationManager
     val currentLocation = MutableLiveData<Location>()
     val isLocationEnabled = MutableLiveData<Boolean>()
     val isLocationPermissionGranted = MutableLiveData<Boolean>()
 
+    /**
+     * startLocationUpdates function initializes the locationManager and starts updating the location if permission is granted.
+     * @param context The context of the calling activity.
+     */
     fun startLocationUpdates(context: Context) {
         locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
@@ -45,14 +53,20 @@ class LocationViewModel : ViewModel(), LocationListener {
             )
             isLocationPermissionGranted.value = true
         }
+        // Check if the GPS_PROVIDER is enabled and update the value
         isLocationEnabled.value = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
     }
 
+    /**
+     * onLocationChanged function is called when there is a change in the location.
+     * @param location The updated location.
+     */
     override fun onLocationChanged(location: Location) {
         currentLocation.value = location
     }
 
     companion object {
+        // Define a constant for the location permission request code
         private const val REQUEST_LOCATION_PERMISSION = 1
     }
 }
