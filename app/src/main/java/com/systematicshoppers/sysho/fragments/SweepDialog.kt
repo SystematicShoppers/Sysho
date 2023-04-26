5837dc94e7310380af6095c5df38792550853b6b
Derek Pearson <dpearson2699@gmail.com>
2023-04-25 20:39:50 -0400

    Added more comments to SweepDialog.kt

--- app/src/main/java/com/systematicshoppers/sysho/fragments/SweepDialog.kt
+++ app/src/main/java/com/systematicshoppers/sysho/fragments/SweepDialog.kt
@@ -10,14 +10,19 @@ import androidx.fragment.app.activityViewModels
 import com.systematicshoppers.sysho.R
 import com.systematicshoppers.sysho.SyshoViewModel
 
-/**A dialog for ApiStoreSelectFragment to increase all prices in a store by a certain percentage.**/
+/**
+ * SweepDialog is a DialogFragment for the ApiStoreSelectFragment to increase all prices in a store by a certain percentage.
+ * The dialog consists of an EditText for entering the percentage value and buttons for confirming and closing the dialog.
+ */
 class SweepDialog: DialogFragment() {
 
+    // UI elements declarations
     private lateinit var activateSweepTextView: TextView
     private lateinit var activateSweepEditTextNumber: EditText
     private lateinit var activateSweepConfirmBtn: Button
     private lateinit var closeActivateSweepDialog: ImageView
 
+    // Access the shared ViewModel
     private val viewModel: SyshoViewModel by activityViewModels()
 
     override fun onCreateView(
@@ -25,35 +30,55 @@ class SweepDialog: DialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.dialog_activate_sweep, container, false)
+
+        // Initialize UI elements
         activateSweepTextView = view.findViewById(R.id.activateSweepTextView)
         activateSweepEditTextNumber = view.findViewById(R.id.activateSweepEditTextNumber)
         activateSweepConfirmBtn = view.findViewById(R.id.activateSweepConfirmBtn)
-        closeActivateSweepDialog = view.findViewById(
-            R.id.closeActivateSweepDialog
-        )
+        closeActivateSweepDialog = view.findViewById(R.id.closeActivateSweepDialog)
+
+        // Request focus on the EditText
         activateSweepEditTextNumber.requestFocus()
+
+        // Variables to store user input
         var inputText: String
         var percent: Double
 
+        // Set a click listener for the close button
         closeActivateSweepDialog.setOnClickListener{
-            dismiss()
+            dismiss()   // Dismiss the dialog
         }
+
+        // Set a click listener for the confirm button
         activateSweepConfirmBtn.setOnClickListener {
+            // Get the input text
             inputText = activateSweepEditTextNumber.text.toString()
+
+            // Check if the input is not empty
             if (inputText.isNotEmpty()) {
+                // Try to convert the input to a double value, or use 0.0 if it's not a valid number
                 percent = inputText.toDoubleOrNull() ?: 0.0
+
+                // Set the sale percentage in the shared ViewModel
                 viewModel.setSalePercent(percent)
+
+                // Show a confirmation dialog
                 SweepDialogConfirm().show(parentFragmentManager, "confirm sale price")
+
+                // Clear the input field
                 activateSweepEditTextNumber.text.clear()
+
+                // Dismiss the dialog
                 dismiss()
             } else {
+                // Clear the input field and show a message if the input is empty
                 activateSweepEditTextNumber.text.clear()
                 Toast.makeText(requireContext(), "Please enter a valid number", Toast.LENGTH_SHORT)
                     .show()
             }
         }
-
         return view
     }
 }
